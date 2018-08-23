652cf9ec458c4fda2736041e3531f637fbb23d7f
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-23 02:59:19 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,10 @@
 var index = require("../index.js")
 
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(index.name).toEqual('Susan')
+    })
+  })
 
   describe('Height', () => {
     it('is less than 40', () => {
